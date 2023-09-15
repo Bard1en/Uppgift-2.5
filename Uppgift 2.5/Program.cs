a39c1247c9d1c12d6a48c2e2b911116c4c4a825c
losc06001 <losc06001@E-PW0234JE>
2023-09-15 13:30:10 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+ namespace Uppgift2._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en multiplication med två decimaltal");
+            string gånger = Console.ReadLine();
+            int mellanslagsIndex = gånger.IndexOf("*");
+            string förstatalet = gånger[..mellanslagsIndex];
+            string andratalet = gånger[(mellanslagsIndex + 1)..];
+
+            double första = double.Parse(förstatalet);
+            double andra = double.Parse(andratalet);
+
+            double sum = första * andra;
+            Console.WriteLine("Dina tal multiplicerade med varandra blir " + sum);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
